4dcf796c4220d1968fe6588e902e579032bbd493
dirkarnez <smalldirkalex@gmail.com>
2023-12-01 18:28:11 +0800

    - upload files

--- Burn_With_Arduino_CLI.cmd
+++ Burn_With_Arduino_CLI.cmd
@@ -13,8 +13,6 @@ if not exist %USERPROFILE%\Downloads\arduino-cli_0.34.2_Windows_64bit (
 cd %USERPROFILE%\Downloads && 	^
 %SystemRoot%\System32\curl.exe "https://github.com/arduino/arduino-cli/releases/download/0.34.2/arduino-cli_0.34.2_Windows_64bit.zip" -L -O &&^
 C:\PROGRA~1\7-Zip\7z.exe x arduino-cli_0.34.2_Windows_64bit.zip -O"%USERPROFILE%\Downloads\arduino-cli_0.34.2_Windows_64bit" &&^
-arduino-cli core download arduino:avr &&^
-arduino-cli.exe core install arduino:avr
 ) else (
 	echo arduino-cli found
 )
@@ -27,5 +25,7 @@ for /f "tokens=*" %%a in (
 )
 
 set PATH=%USERPROFILE%\Downloads\arduino-cli_0.34.2_Windows_64bit;
+arduino-cli core download arduino:avr &&^
+arduino-cli.exe core install arduino:avr &&^
 arduino-cli upload -p %output% --fqbn  arduino:avr:uno --input-file=EIE3105_ATmega328P_Application.hex
 pause
--- main.c
+++ main.c
@@ -1,34 +1,80 @@
-/*
- * EIE3105_Lab2_A_2.c
- *
- * Created: 2018-09-21 3:02:30 PM
- * Author : Bao
- */ 
-
 #include <avr/io.h>
+#include <string.h>
 
-void Timer_1_Delay();		// Prototype for Delay Function 
+void usart_init()
+{
+	UCSR0B = (1<<TXEN0) | (1<<RXEN0);
+	UCSR0C = (1<<UCSZ00) | (1<<UCSZ01);
+	UBRR0L = 0xCF; 
+}
+
+void usart_send(unsigned char ch)
+{
+	while(!(UCSR0A &(1<<UDRE0)));
+	UDR0 = ch;
+}
+
+void usart_read(unsigned char* ptr_ch)
+{
+	while(!(UCSR0A & (1<<RXC0)));
+	*ptr_ch = UDR0;
+}
+
+
+void print(char* message, size_t length) {
+	for (unsigned char i = 0; i <= length; i++) {
+		usart_send(*(message + i));
+	}
+}
 
 int main(void)
 {
-    DDRB = 1<<0;			// PB0 as Output
-	PORTB = 1<<0;			// Initial LOW
-    while (1)				// INF Loop
-    {
-		Timer_1_Delay();	// Call 1 s Delay
-		PORTB ^= (1<<0);	// Toggle
-    }
-}
+	DDRB = 0x00;
+	
+	DDRD = 0xFF;
+	PORTD = 0x00;
+	
+	unsigned char last_state = 0;
+	unsigned char current_pb = 0;
+	unsigned char previous_pb = 0;
+	char o[] = "o\n";
+	char f[] = "f\n";
 
-void Timer_1_Delay()
-{
-// Timer 1 16 bit timer with prescaler = 1024
-// 1 * 16M / 1024 = 15625
-	OCR1AH = 0x3D;		// CTC OCR
-	OCR1AL = 0x08;		// 15625 - 1 = 0x3D08
-	TCCR1A = 0x00;		// CTC Mode
-	TCCR1B = 0x0D;		// CTC Mode, Prescaler = 1024
-	while ((TIFR1 &(1<<OCF1A))==0);	// Continue Until Overflow
-	TCCR1B = 0x00;		// Stop Timer 1
-	TIFR1 = (1<<OCF1A);	// Reset OCR1A
+	usart_init();
+
+	while (1) {
+		current_pb = PINB;
+		
+		
+		if (((current_pb > 1) && ((previous_pb & 0x10) == 0) )) {
+			print(o, strlen(o));
+
+			if (last_state == 0) {
+				last_state = 1;
+				PORTD = 0xFF;
+			} else if (last_state == 1) {
+				last_state = 0;
+				PORTD = 0x00;
+			}
+		} else {
+			print(f, strlen(f));
+		}
+		previous_pb = current_pb;
+	}
+	
+	// while (1) {
+	// 	current_pb = PINB;
+		
+		
+	// 	if (((current_pb & 0x10) == 0x10) && ((previous_pb & 0x10) == 0x00) ) {
+	// 		if (last_state == 0) {
+	// 			last_state = 1;
+	// 			PORTD = 0xFF;
+	// 		} else if (last_state == 1) {
+	// 			last_state = 0;
+	// 			PORTD = 0x00;
+	// 		}
+	// 	}
+	// 	previous_pb = current_pb;
+	// }
 }
